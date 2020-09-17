803eae9dea10191a6ca4a9d245f07eb37b19abb1
ksbrazier <kylebrazier@gmail.com>
2020-09-17 15:09:15 -0400

    npm

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const { start } = require("repl");
 const connection = mysql.createConnection({
     host: "localhost",
     port: 8080,
